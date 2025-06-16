f65ec6d9afddf9241a9c1b745b3e44c81212fd2c
unknown <duyzfclong@gmail.com>
2025-06-16 15:58:43 +0700

    Set up client

---
+++ frontend/client/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      Hello cả lò
+    </>
+  )
+}
+
+export default App
---
+++ frontend/client/src/translate/i18n.ts
@@ -0,0 +1,16 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    resources: {
+      en: { translation: { hello: 'Hello' } },
+      vi: { translation: { hello: 'Xin chào' } },
+    },
+  })
+
+export default i18n
